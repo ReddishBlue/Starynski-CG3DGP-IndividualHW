e7d795e82f44eecaccf9d49a7d9ecbb98f8eaef7
t. starynski <toristarynski@gmail.com>
2025-09-12 16:03:06 -0400

    New Input System Package has been added, and player input updated to use that system instead of the old one. Ch 6 completed

--- CG3DGP-HW/Assets/Scripts/PlayerShooting.cs
+++ CG3DGP-HW/Assets/Scripts/PlayerShooting.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
     [SerializeField] private GameObject shootPoint;
 
-    // Start is called before the first frame update
-    void Start()
+
+    public void OnFire(InputValue value)
     {
-        
+        if (value.isPressed)
+        {
+            GameObject clone = Instantiate(bulletPrefab);
+
+            clone.transform.position = shootPoint.transform.position;
+            clone.transform.rotation = shootPoint.transform.rotation;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) //Mouse0 = left click, Mouse1 = right click, Mouse2 = middle click
-        {
-            Instantiate(bulletPrefab, shootPoint.transform.position, shootPoint.transform.rotation);
-        }
+        // =========== New Input System ============\\
+
+        // =========== Old Input System ============\\
+        // if (Input.GetKeyDown(KeyCode.Mouse0)) //Mouse0 = left click, Mouse1 = right click, Mouse2 = middle click
+        // {
+        //     Instantiate(bulletPrefab, shootPoint.transform.position, shootPoint.transform.rotation);
+        // }
     }
 }
