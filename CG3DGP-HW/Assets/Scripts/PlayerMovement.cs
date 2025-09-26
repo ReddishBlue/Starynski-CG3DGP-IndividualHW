447ddef7d4d62d59685505b7fcbf0305e6692725
t. starynski <toristarynski@gmail.com>
2025-09-26 18:46:49 -0400

    player has physics now and friction and their own physics materal. chapter 7 is complete

--- CG3DGP-HW/Assets/Scripts/PlayerMovement.cs
+++ CG3DGP-HW/Assets/Scripts/PlayerMovement.cs
@@ -11,11 +11,14 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movementValue;
     private float lookValue;
     private Rigidbody rb;
+    //im keeping it as rb and not rigidbody despite what the book said because im super prone to spelling errors
 
     private void Awake()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        rb = GetComponent<Rigidbody>();
     }
 
     public void OnMove(InputValue value)
@@ -30,20 +33,16 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        // =========== With Rigidbodies ============\\
+        rb.AddRelativeForce(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
+        rb.AddRelativeTorque(0, lookValue * Time.deltaTime, 0);
 
         // =========== New Input System ============\\
-        transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
-        transform.Rotate(0, lookValue * Time.deltaTime, 0);
+        // transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
+        // transform.Rotate(0, lookValue * Time.deltaTime, 0);
 
         // =========== Old Input System ============\\
-        // if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) { transform.Translate(0, 0, speed * Time.deltaTime); }
-        // if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) { transform.Translate(0, 0, -speed * Time.deltaTime); }
-        // if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(-speed * Time.deltaTime, 0, 0); }
-        // if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) { transform.Translate(speed * Time.deltaTime, 0, 0); }
-
-        // //player rotation to follow the mouse
-        // float mouseX = Input.GetAxis("Mouse X");
-        // transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0); //y is our vertical so to rotate in the xz plane rotate around ys
+        // deleted for clarity!
 
     }
 }
